8921759d167967427cfacc75b22dbe3278d10311
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-24 14:45:01 -0500

    Feat: added ability to translate message.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,22 +1,21 @@
+require './lib/dictionary'
+
+dictionary = Dictionary.new
+
 read_file = File.open(ARGV[0], "r")
-
-english_text = read_file.read
-
-read_file.close
-
-puts english_text
-
-braille_text = english_text.upcase
-
 write_file = File.open(ARGV[1], "w")
 
-write_file.write(braille_text)
+eng_text = read_file.read
+braille_text = dictionary.translate(eng_text)
 
+write_file.write(braille_text)
 write_file.close
 
-puts "Created 'braille.txt' containing #{english_text.length} characters"
+puts "Created #{ARGV[1]} containing #{eng_text.length - 1} characters"
 
 
 # english = File.open(ARGV[0],"r")
-# read_english = english.read
+# read_english = english.read.chomp
 # braille = File.open(ARGV[1],"w")
+# translate = Translate.new(read_english, braille)
+# require 'pry';binding.pry

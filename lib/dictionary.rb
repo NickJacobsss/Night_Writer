ef5e6698e163e1849aa3152523649e53e1e5a993
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-24 17:27:36 -0500

    Refactor: made dictionary a module, and moved tests to translator class

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,10 +1,4 @@
-class Dictionary
-  attr_reader :eng_to_braille_hash,:output_text
-
-  def initialize
-    @eng_to_braille_hash = eng_to_braille_hash
-    @output_text = ""
-  end
+module Dictionary
 
   def eng_to_braille_hash
     {
@@ -36,38 +30,5 @@ class Dictionary
       "z" => ["O.", ".O", "OO"],
       " " => ["..", "..", ".."],
     }
-  end
-
-  def character_to_braille(input)
-    @eng_to_braille_hash[input]
-  end
-
-  def text_convert(input)
-    input.chars.map {|char| character_to_braille(char)}.compact
-  end
-
-  def top_row(input)
-    top_row = text_convert(input).map {|letter| letter[0]}.join
-  end
-
-  def middle_row(input)
-    text_convert(input).map {|letter| letter[1]}.join
-  end
-
-  def bottom_row(input)
-    text_convert(input).map {|letter| letter[2]}.join
-  end
-
-  def translate(input)
-    if input.length <= 40
-      lines_arr = [top_row(input),middle_row(input),bottom_row(input)]
-      @output_text += lines_arr.map {|line| line + "\n"}.join
-    else
-      before_40_input = input[0..39]
-      lines_arr = [top_row(before_40_input),middle_row(before_40_input),bottom_row(before_40_input)]
-      @output_text += lines_arr.map {|line| line + "\n"}.join
-      after_40_input = input[40..-1]
-      translate(after_40_input)
-    end
   end
 end

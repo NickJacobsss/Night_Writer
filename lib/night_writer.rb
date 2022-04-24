1acf9e3bbed9771b0d2d95f563f5813a615dd2a2
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-24 16:36:44 -0500

    Fix: updated print statement

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,16 +6,9 @@ read_file = File.open(ARGV[0], "r")
 write_file = File.open(ARGV[1], "w")
 
 eng_text = read_file.read
-braille_text = dictionary.translate(eng_text)
+braille_text = dictionary.translate(eng_text.downcase)
 
 write_file.write(braille_text)
 write_file.close
 
 puts "Created #{ARGV[1]} containing #{eng_text.length - 1} characters"
-
-
-# english = File.open(ARGV[0],"r")
-# read_english = english.read.chomp
-# braille = File.open(ARGV[1],"w")
-# translate = Translate.new(read_english, braille)
-# require 'pry';binding.pry

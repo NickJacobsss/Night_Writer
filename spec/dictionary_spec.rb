5b355b7c169abca93d2bbd409fcb111d20de450d
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-24 14:46:07 -0500

    Test: added tests for rows/translate methods

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -9,12 +9,43 @@ RSpec.describe Dictionary do
 
   it "has english keys and corresponding braille values" do
     dictionary = Dictionary.new
-    expect(dictionary.english_conversion["h"]).to eq(["O.", "OO", ".."])
-    expect(dictionary.english_conversion["e"]).to eq(["O.", ".O", ".."])
-    expect(dictionary.english_conversion["l"]).to eq(["O.", "O.", "O."])
-    expect(dictionary.english_conversion["l"]).to eq(["O.", "O.", "O."])
-    expect(dictionary.english_conversion["o"]).to eq(["O.", ".O", "O."])
+    expect(dictionary.eng_to_braille_hash["h"]).to eq(["O.", "OO", ".."])
+    expect(dictionary.eng_to_braille_hash["e"]).to eq(["O.", ".O", ".."])
+    expect(dictionary.eng_to_braille_hash["l"]).to eq(["O.", "O.", "O."])
+    expect(dictionary.eng_to_braille_hash["l"]).to eq(["O.", "O.", "O."])
+    expect(dictionary.eng_to_braille_hash["o"]).to eq(["O.", ".O", "O."])
   end
 
+  it "can take a letter and convert to braille" do
+    dictionary = Dictionary.new
+    expect(dictionary.character_to_braille("a")).to eq(["O.", "..", ".."])
+  end
+
+  it "can get input and splitting into array of characters" do
+    dictionary = Dictionary.new
+    expect(dictionary.text_convert("ab")).to eq([["O.", "..", ".."], ["O.", "O.", ".."]])
+  end
+
+  it "can split english letters into top row braille only" do
+    dictionary = Dictionary.new
+    expect(dictionary.top_row("ab")).to eq("O.O.")
+  end
+
+  it "can split english letters into middle row braille only" do
+    dictionary = Dictionary.new
+    expect(dictionary.middle_row("ab")).to eq("..O.")
+  end
+
+  it "can split english letters into bottom row braille only" do
+    dictionary = Dictionary.new
+    expect(dictionary.bottom_row("ab")).to eq("....")
+  end
+
+  it "can print out input in 3 rows" do
+    dictionary = Dictionary.new
+    user_input = "ab"
+    # require 'pry';binding.pry
+    expect(dictionary.translate(user_input)).to eq("O.O.\n..O.\n....\n")
+  end
 
 end

4ebe0e91792a5105738f53023de8fddc38b7447d
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-24 14:46:31 -0500

    Feat: Added row & translate methods

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,7 +1,12 @@
 class Dictionary
+  attr_reader :eng_to_braille_hash
 
-  def english_conversion
-    @dictionary_hash = {
+  def initialize
+    @eng_to_braille_hash = eng_to_braille_hash
+  end
+
+  def eng_to_braille_hash
+    {
       "a" => ["O.", "..", ".."],
       "b" => ["O.", "O.", ".."],
       "c" => ["OO", "..", ".."],
@@ -28,6 +33,42 @@ class Dictionary
       "x" => ["OO", "..", "OO"],
       "y" => ["OO", ".O", "OO"],
       "z" => ["O.", ".O", "OO"],
+      " " => ["..", "..", ".."],
     }
+  end
+
+  def character_to_braille(input)
+    @eng_to_braille_hash[input]
+  end
+
+  def text_convert(input)
+    input.chars.map do |char|
+      character_to_braille(char)
+    end.compact
+  end
+
+  def top_row(input)
+    text_convert(input).map do |letter|
+      letter[0]
+    end.join
+  end
+
+  def middle_row(input)
+    text_convert(input).map do |letter|
+      letter[1]
+    end.join
+  end
+
+  def bottom_row(input)
+    text_convert(input).map do |letter|
+      letter[2]
+    end.join
+  end
+
+  def translate(input)
+    lines_arr = [top_row(input),middle_row(input),bottom_row(input)]
+    lines_arr.map do |line|
+      line + "\n"
+    end.join
   end
 end

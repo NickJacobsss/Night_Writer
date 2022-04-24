0fc3df7413ba06a5df1f9c888eb63282d6eb8751
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-24 16:36:00 -0500

    Feat: added functionality to translate for messages over 80 characters

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,8 +1,9 @@
 class Dictionary
-  attr_reader :eng_to_braille_hash
+  attr_reader :eng_to_braille_hash,:output_text
 
   def initialize
     @eng_to_braille_hash = eng_to_braille_hash
+    @output_text = ""
   end
 
   def eng_to_braille_hash
@@ -42,33 +43,31 @@ class Dictionary
   end
 
   def text_convert(input)
-    input.chars.map do |char|
-      character_to_braille(char)
-    end.compact
+    input.chars.map {|char| character_to_braille(char)}.compact
   end
 
   def top_row(input)
-    text_convert(input).map do |letter|
-      letter[0]
-    end.join
+    top_row = text_convert(input).map {|letter| letter[0]}.join
   end
 
   def middle_row(input)
-    text_convert(input).map do |letter|
-      letter[1]
-    end.join
+    text_convert(input).map {|letter| letter[1]}.join
   end
 
   def bottom_row(input)
-    text_convert(input).map do |letter|
-      letter[2]
-    end.join
+    text_convert(input).map {|letter| letter[2]}.join
   end
 
   def translate(input)
-    lines_arr = [top_row(input),middle_row(input),bottom_row(input)]
-    lines_arr.map do |line|
-      line + "\n"
-    end.join
+    if input.length <= 40
+      lines_arr = [top_row(input),middle_row(input),bottom_row(input)]
+      @output_text += lines_arr.map {|line| line + "\n"}.join
+    else
+      before_40_input = input[0..39]
+      lines_arr = [top_row(before_40_input),middle_row(before_40_input),bottom_row(before_40_input)]
+      @output_text += lines_arr.map {|line| line + "\n"}.join
+      after_40_input = input[40..-1]
+      translate(after_40_input)
+    end
   end
 end
